a303b6ca869876c1fad8cf974ddb52434fa05e2a
ivkopylovv <vanyakopylovv@mail.ru>
2022-06-17 18:00:31 +0300

    first commit: initialize project

---
+++ src/main/java/com/kopylov/musicplatform/MusicPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.kopylov.musicplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kopylov/musicplatform/MusicPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kopylov.musicplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
