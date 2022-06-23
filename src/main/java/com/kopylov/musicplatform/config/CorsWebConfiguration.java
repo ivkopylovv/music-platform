2ec6f8bca44c3d9830c6919484fbaad868985f78
ivkopylovv <vanyakopylovv@mail.ru>
2022-06-23 20:56:35 +0300

    add: CorsWebConfiguration

---
+++ src/main/java/com/kopylov/musicplatform/config/CorsWebConfiguration.java
@@ -0,0 +1,16 @@
+package com.kopylov.musicplatform.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+public class CorsWebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:4210")
+                .allowCredentials(true);
+    }
+}
